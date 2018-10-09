72a13dd8ad76f95eb326011bf21247d21a43bd83
Boon Tek <darkon.garon@gmail.com>
2018-10-09 10:45:55 +0800

    1. added index, create,show for locks_controller.rb

--- app/controllers/locks_controller.rb
+++ app/controllers/locks_controller.rb
@@ -1,19 +1,31 @@
 class LocksController < ApplicationController
   def index
+    @locks = Lock.where(user_id: params[:user_id])
   end
 
   def new
   end
 
   def create
+    @lock = Lock.new(lock_params)
+    @lock.user_id = current_user.id
+    @lock.save
+    redirect_to user_locks_path
+
   end
 
   def show
+    @lock = Lock.find(params[:id])
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+  def lock_params
+    params.require(:lock).permit(:lock_name, :location, :group)
   end
 end
