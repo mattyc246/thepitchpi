ecda97cdd924eacaf6bc35db30daa71e2e366818
notedharma <notedharma@gmail.com>
2018-10-09 18:04:15 +0800

    map work in progress1, added to db and gems

--- app/models/lock.rb
+++ app/models/lock.rb
@@ -2,6 +2,9 @@ class Lock < ApplicationRecord
   belongs_to :user
 
   validates :lock_name, presence: true
+  reverse_geocoded_by :latitude, :longitude,
+          :address => :location
+          after_validation :reverse_geocode
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_081848) do
+ActiveRecord::Schema.define(version: 2018_10_09_063013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,11 +18,13 @@ ActiveRecord::Schema.define(version: 2018_10_08_081848) do
   create_table "locks", force: :cascade do |t|
     t.string "lock_name", default: "New Lock", null: false
     t.boolean "status", default: true, null: false
-    t.decimal "location"
+    t.string "location"
     t.string "group"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.float "longitude"
+    t.float "latitude"
     t.index ["user_id"], name: "index_locks_on_user_id"
   end
 
