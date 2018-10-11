db64fe4e6a39fbd2c88f2310db9014336b4be06a
Boon Tek <darkon.garon@gmail.com>
2018-10-11 14:56:32 +0800

    1. now slider status reflects to whatever is stated in db table locks column status 2. unlocked status colour changed from grey to red 3. to remove obsolete method lock_status

--- app/controllers/locks_controller.rb
+++ app/controllers/locks_controller.rb
@@ -1,4 +1,5 @@
 class LocksController < ApplicationController
+  
   def index
     @locks = Lock.where(user_id: params[:user_id])
   end
@@ -11,7 +12,6 @@ class LocksController < ApplicationController
     @lock.user_id = current_user.id
     @lock.save
     redirect_to user_locks_path
-
   end
 
   def show
@@ -29,21 +29,14 @@ class LocksController < ApplicationController
     @lock_status = ""
 
     host = ENV['RASPBERRY_PI_HOST']
-
     user = ENV['RASPBERRY_PI_USER']
-
     password = ENV['RASPBERRY_PI_PASSWORD']
 
     Net::SSH.start(host, user, password: password) do |ssh|
-      
       output = ssh.exec!("cd Desktop; python button_press.py")
-       
       @lock_status = output.split
-
     end
-    
     render :json => {status: @lock_status[0].to_s}
-
   end
 
   def toggle_lock
@@ -53,61 +46,38 @@ class LocksController < ApplicationController
     @lock_status = ""
 
     if lock.status == true
-
       lock.status = false
-
       if lock.save
-      
         host = ENV['RASPBERRY_PI_HOST']
-
         user = ENV['RASPBERRY_PI_USER']
-
         password = ENV['RASPBERRY_PI_PASSWORD']
 
         Net::SSH.start(host, user, password: password) do |ssh|
-
           output = ssh.exec!("cd Desktop; python button_press.py")
-           
           @lock_status = output.split
-
           if @lock_status[0].to_s == "Locked"
-          
             ssh.exec!("cd Desktop; python lock_on.py")
-
             render json: {notice: "Lock has been unlocked"}
-
           else
-
             render json: {notice: "Error! Unable to unlock door!"}
-
           end
-
         end
-      
-        
-      
       else
-
         render json: {notice: "Error! Please try again!"}
-
       end
-
-
     else
-
       lock.status = true
-
       if lock.save
         render json: {notice: "Lock has been locked!"}
       else
         render json: {notice: "Error! Please try again!"}
       end
-
     end
   end
 
   private
   def lock_params
-    params.require(:lock).permit(:lock_name, :location, :group)
+    params.require(:locks).permit(:lock_name, :location, :group)
   end
+
 end
