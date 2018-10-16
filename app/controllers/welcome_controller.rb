1dadec3ab3df4d9ad21563b797c3912b104f71dc
mattyc246 <mattyc246@gmail.com>
2018-10-16 10:09:33 +0800

    1. Added easter egg

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,5 +9,20 @@ class WelcomeController < ApplicationController
   	end
 
   end
+
+  def easter_egg
+
+  	if current_user && current_user.subscription_status == true
+
+  		@user = User.find(current_user.id)
+
+  	else
+
+  		flash[:notice] = "You are not ready to be a lockstar!"
+  		redirect_to root_path
+  		
+  	end
+
+  end
   
 end
